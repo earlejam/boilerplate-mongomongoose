b3770ce50d778c572bb6e3cfeea79208d2974933
James Earley <jamesearley22@gmail.com>
2025-03-20 16:09:22 -0500

    Add Person mongoose model

--- myApp.js
+++ myApp.js
@@ -4,7 +4,23 @@ let mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-let Person;
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  favoriteFoods: {
+    type: [String],
+    required: false
+  }
+});
+
+// attach model definition to named object
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
